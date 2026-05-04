a3d348f68d1228aab783503e288d710b280788bc
dvy19 <divyadixit.311204@gmail.com>
2026-05-04 18:29:25 +0530

    setup

---
+++ users/serializers.py
@@ -0,0 +1,48 @@
+
+from rest_framework import serializers
+from .models import CustomUser
+
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+class RegisterSerializer(serializers.ModelSerializer):
+
+    # write_only = password will never be sent back in response
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model  = CustomUser
+        fields = ["email", "password", "role"]
+
+    def create(self, validated_data):
+        # Use our custom manager to create the user
+        user = CustomUser.objects.create_user(
+            email    = validated_data["email"],
+            password = validated_data["password"],
+            role     = validated_data["role"],
+        )
+        return user
+    
+
+class LoginSerializer(serializers.Serializer):
+
+    email    = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+
+        # Generate tokens
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "user": user,
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+            "role": user.role
+        }
