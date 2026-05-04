ffe209df2d599e95d5d781d963edce1eb1b022a1
dvy19 <divyadixit.311204@gmail.com>
2026-05-04 18:54:04 +0530

    changes in serializers

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from serializers import LoginSerializer, RegisterSerializer# Create your views here.
+from .serializers import LoginSerializer, RegisterSerializer# Create your views here.
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 
