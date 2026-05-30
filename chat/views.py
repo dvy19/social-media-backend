fd6e0fb5ada917f6e10ffab0db6aec8fbd42dec3
dvy19 <divyadixit.311204@gmail.com>
2026-05-30 14:07:24 +0530

    change in imports

--- chat/views.py
+++ chat/views.py
@@ -10,7 +10,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from serializer import MessageSerializer
+from .serializer import MessageSerializer
 
 from .models import Conversation
 
